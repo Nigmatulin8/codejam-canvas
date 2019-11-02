39df536ab0a4c4a0690c589ae23688926469601b
Ali Gasymov <alik0211alik@gmail.com>
2019-11-02 17:07:00 +0400

    111

--- src/index.js
+++ src/index.js
@@ -3,80 +3,57 @@ import './styles.scss';
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 
-const resolution = document.getElementsByClassName('resolution')[0];
-
-//From HTMLCollection to Array
-const buttons =  [...document.getElementsByClassName('item')];
-
-const sizeEl = {
-	'4x4': 128,
-	'32x32': 16 
+const hex2rgba = (hex, alpha = 1) => {
+	const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+	return `rgba(${r},${g},${b},${alpha})`;
 };
 
-const URL = {
-	json_1: './src/data/4x4.json',
-	json_2: './src/data/32x32.json',
-	img_1: './src/data/image.png'
-};
+function drawPixel(canvas, startX, startY, sizePixel, color) {
+	if (color.length === 6) {
+		canvas.fillStyle = hex2rgba(color);
+	} else {
+		canvas.fillStyle = `rgba(${color})`;
+	}
+	canvas.fillRect(startX, startY, sizePixel, sizePixel);
+}
 
-getData(URL).then(data => {
-	resolution.addEventListener('click', e => {
-		if (e.target.dataset.size) {
-			let size = e.target.dataset.size; //4x4 | 32x32 | 256x256
-		
-			buttons.forEach(item =>  item.classList.remove('active'));
+function drawImage(canvas, dataArray) {
+	let sizePixel = 0;
+	let x = 0;
+	let y = 0;
 
-			e.target.classList.add('active');
+	sizePixel = 512 / dataArray.length;
 
-			if(size == '256x256') {
-				const pic = new Image();
-				pic.src = URL.img_1;
-				
-				pic.onload = () => {
-					context.drawImage(pic, 0, 0, 512, 512);
-				};
-			}
+	for (let row of dataArray) {
+		for (let cell of row) {
+			drawPixel(canvas, x, y, sizePixel, cell);
+			x += sizePixel;
+		}
+		y += sizePixel;
+		x = 0;
+	}
+}
 
-			else { draw(data[size], sizeEl[size]); }
+document.querySelectorAll('.actions__item').forEach(item => {
+	item.addEventListener('click', () => {
+
+		const { type, url } = item.dataset;
+
+		switch (type) {
+			case 'pixels':
+				fetch(url).then(response => response.json()).then(data => drawImage(context, data));
+				break;
+
+			case 'image':
+				const img = new Image();
+				img.onload = function () {
+					context.drawImage(img, 0, 0, 512, 512);
+				}
+				img.src = url;
+				break;
+
+			default:
+				break;
 		}
 	});
 });
-
-async function getData (url) {
-	const data = {};
-
-	for (let key in url) {
-		if(url.hasOwnProperty(key) && key.match(/json/)) {
-			const response = await fetch(url[key]);
-
-			/* 
-			Создание объекта data типа:
-			obj {
-				4x4: array,
-				32x32: array
-			}
-
-			url[key].match(/\d[x]\d/g) => 4x4 or 32x32
-			*/
-
-			data[url[key].match(/\d{1,6}[x]\d{1,6}/g)] = await response.json();
-		}
-	}
-
-	return data;
-}
-
-function draw(imgColors, size){
-	imgColors.forEach(function(row, indexRow) {
-		row.forEach( function(column, indexColumn) {
-			if(Array.isArray(column)){
-				context.fillStyle = `rgba(${column[0]}, ${column[1]}, ${column[2]}, ${column[3]})`;
-			} 
-			else {
-				context.fillStyle = `#${column}`;
-			}
-
-			context.fillRect(indexColumn * size, indexRow * size, size, (indexColumn + 1) * size);
-		});        
-	});
-}
